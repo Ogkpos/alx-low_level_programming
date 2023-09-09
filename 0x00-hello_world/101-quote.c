240e84e6b5aa10fa25d6856464157144725c3cf5
Ogkpos <evwerhamreisrael@gmail.com>
2023-09-09 08:57:32 -0700

    prints a line to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: A c program to print a line to standard error
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n");
+	return (1);
+}
