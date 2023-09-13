277633ad15f15553b4ecb3c946173a277519da36
Ogkpos <evwerhamreisrael@gmail.com>
2023-09-13 13:13:51 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Program to print _putchar
+ *
+ * Return: Always 0 (Success);
+ */
+
+int main(void)
+{
+	_putchar('u');
+	_putchar('p');
+	_putchar('i');
+	_putchar('\n');
+	return (0);
+}
+
