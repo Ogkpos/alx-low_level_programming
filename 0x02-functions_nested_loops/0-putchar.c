9dbc04a65ecae28445856e55e52624d5d4460367
Ogkpos <evwerhamreisrael@gmail.com>
2023-09-13 13:18:19 -0700

    9 chars long

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,6 +13,12 @@ int main(void)
 	_putchar('u');
 	_putchar('p');
 	_putchar('i');
+	_putchar('o');
+	_putchar('y');
+	_putchar('x');
+	_putchar('m');
+	_putchar('z');
+	_putchar('b');
 	_putchar('\n');
 	return (0);
 }
